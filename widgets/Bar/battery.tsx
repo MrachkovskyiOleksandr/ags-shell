2dbc48b24536ec6baa1d2dbf4bc8a4dd52d6e911
MrachkovskyiOleksandr <oleksandrmrackovskyj@gmail.com>
2026-03-13 07:53:01 +0100

    battery icon fix

--- widgets/Bar/battery.tsx
+++ widgets/Bar/battery.tsx
@@ -1,7 +1,20 @@
 import AstalBattery from "gi://AstalBattery"
-import { createBinding } from "ags"
+
+import { createBinding, createState } from "ags"
 import { createPoll } from "ags/time"
 import { Gdk } from "ags/gtk4"
+
+const getBatteryIcon = (percent: number, charging: boolean) => {
+  if (percent >= 100)
+    return charging
+      ? "battery-level-100-charged-symbolic"
+      : "battery-level-100-symbolic"
+
+  const level = Math.round(percent / 10) * 10
+  return charging
+    ? `battery-level-${level}-charging-symbolic`
+    : `battery-level-${level}-symbolic`
+}
 
 export default function Battery() {
   const battery = AstalBattery.get_default()
@@ -11,15 +24,29 @@ export default function Battery() {
     "percentage",
   )((p) => `${Math.floor(p * 100)}%`)
 
+  const [icon, setIcon] = createState(
+    getBatteryIcon(Math.round(battery.percentage * 100), battery.charging),
+  )
+
+  battery.connect("notify::percentage", () =>
+    setIcon(
+      getBatteryIcon(Math.round(battery.percentage * 100), battery.charging),
+    ),
+  )
+  battery.connect("notify::charging", () =>
+    setIcon(
+      getBatteryIcon(Math.round(battery.percentage * 100), battery.charging),
+    ),
+  )
+
   const tooltipText = createPoll("", 3000, () => {
-    const isCharging = battery.charging 
-    
+    const isCharging = battery.charging
+
     if (!isCharging) {
       const h = Math.floor(battery.timeToEmpty / 3600)
       const m = Math.floor((battery.timeToEmpty % 3600) / 60)
       return `Empty in ${h}:${String(m).padStart(2, "0")}`
-    } 
-    else {
+    } else {
       const h = Math.floor(battery.timeToFull / 3600)
       const m = Math.floor((battery.timeToFull % 3600) / 60)
       return `Full in ${h}:${String(m).padStart(2, "0")}`
@@ -32,7 +59,7 @@ export default function Battery() {
       cssClasses={["battery-box"]}
       tooltipText={tooltipText}
     >
-      <image iconName={createBinding(battery, "iconName")} />
+      <image iconName={icon} />
       <label label={percent} />
     </box>
   )
