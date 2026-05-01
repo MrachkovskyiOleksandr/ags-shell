22d345c9c65585499796d28f5790cbdc5de7beaf
MrachkovskyiOleksandr <oleksandrmrackovskij71@gmail.com>
2026-05-01 05:41:52 +0200

    feat: slider for screen brightness

---
+++ utils/display.ts
@@ -0,0 +1,13 @@
+import { createState } from "ags"
+import { execAsync } from "ags/process"
+import { createPoll } from "ags/time"
+
+export const [max, setMax] = createState(1)
+export const [percentage, setPercentage] = createState(1)
+
+execAsync("brightnessctl max").then((v) => setMax(Number(v)))
+
+export const brightness = createPoll(0, 200, async (prev) => {
+  const v = await execAsync("brightnessctl get")
+  return Number(v)
+})
