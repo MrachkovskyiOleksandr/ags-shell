69b5f8b2496e7fcbf02246055b387358083a0f02
MrachkovskyiOleksandr <oleksandrmrackovskij71@gmail.com>
2026-05-02 07:18:00 +0200

    feat/style: brightness persentage and layout rework

--- utils/display.ts
+++ utils/display.ts
@@ -1,9 +1,8 @@
-import { createState } from "ags"
+import { createMemo, createState } from "ags"
 import { execAsync } from "ags/process"
 import { createPoll } from "ags/time"
 
 export const [max, setMax] = createState(1)
-export const [percentage, setPercentage] = createState(1)
 
 execAsync("brightnessctl max").then((v) => setMax(Number(v)))
 
@@ -11,3 +10,5 @@ export const brightness = createPoll(0, 200, async (prev) => {
   const v = await execAsync("brightnessctl get")
   return Number(v)
 })
+
+export const percentage = createMemo(() => Math.floor((brightness() / max()) * 100))
--- widgets/Status-Bar/control.tsx
+++ widgets/Status-Bar/control.tsx
@@ -3,11 +3,12 @@ import { Gtk } from "ags/gtk4"
 import { pointer } from "../../utils/format"
 import { audioIcon, device } from "../../utils/audio"
 import { execAsync } from "ags/process"
-import { brightness, max } from "../../utils/display"
+import { brightness, max, percentage } from "../../utils/display"
+import { MyPopover } from "../../utils/custom-elements"
 
 export default function Control() {
   const volume = createBinding(device, "volume").as((b) => {
-    return `Volume: ${Math.floor(b * 100)}%`
+    return `${Math.floor(b * 100)}`
   })
 
   return (
@@ -17,64 +18,62 @@ export default function Control() {
           <image iconName={"display-brightness"} />
           <image iconName={audioIcon} />
         </box>
-        <popover hasArrow={false}>
-          <box orientation={Gtk.Orientation.VERTICAL}>
+        <MyPopover hasArrow={false} yoffset={14}>
+          <box orientation={Gtk.Orientation.VERTICAL} spacing={8}>
             <box
               cssClasses={["audio-section"]}
-              orientation={Gtk.Orientation.VERTICAL}
+              orientation={Gtk.Orientation.HORIZONTAL}
+              spacing={4}
             >
-              <label cssClasses={["header"]} label={volume}></label>
-              <box orientation={Gtk.Orientation.HORIZONTAL} spacing={4}>
-                <slider
-                  hexpand
-                  cursor={pointer}
-                  value={createBinding(device, "volume")}
-                  min={0}
-                  max={1}
-                  onChangeValue={({ value }) => device.set_volume(value)}
-                />
-                <button
-                  cssClasses={createBinding(device, "mute").as((m) =>
-                    m ? ["mute-button", "muted"] : ["mute-button"],
-                  )}
-                  cursor={pointer}
-                  onClicked={() => {
-                    device.set_mute(!device.mute)
-                  }}
-                >
-                  <image iconName={audioIcon}></image>
-                </button>
-              </box>
+              <label cssClasses={["percentage"]} label={volume}></label>
+              <slider
+                hexpand
+                cursor={pointer}
+                value={createBinding(device, "volume")}
+                min={0}
+                max={1}
+                onChangeValue={({ value }) => device.set_volume(value)}
+              />
+              <button
+                cssClasses={createBinding(device, "mute").as((m) =>
+                  m ? ["mute-button", "muted"] : ["mute-button"],
+                )}
+                cursor={pointer}
+                onClicked={() => {
+                  device.set_mute(!device.mute)
+                }}
+              >
+                <image iconName={audioIcon}></image>
+              </button>
             </box>
             <box
               cssClasses={["screen-section"]}
-              orientation={Gtk.Orientation.VERTICAL}
+              orientation={Gtk.Orientation.HORIZONTAL}
+              spacing={4}
             >
               <label
-                cssClasses={["header"]}
-                label={""}
+                cssClasses={["percentage"]}
+                label={percentage.as((p) => p.toString())}
               ></label>
-              <box orientation={Gtk.Orientation.HORIZONTAL} spacing={4}>
-                <slider
-                  hexpand
-                  cursor={pointer}
-                  value={brightness}
-                  min={0}
-                  max={max}
-                  onChangeValue={(self, _scrollType, value) =>
-                    void execAsync(`brightnessctl set ${Math.round(value)}`)
-                  }
-                  $={(self) => {
-                    self.set_increments(5, 5)
-                  }}
-                />
-                <button cssClasses={["night-light-button"]}>
-                  <image iconName={"night-light"} />
-                </button>
-              </box>
+              <slider
+                hexpand
+                cursor={pointer}
+                value={brightness}
+                min={0}
+                max={max}
+                onChangeValue={(self, _scrollType, value) =>
+                  void execAsync(`brightnessctl set ${Math.round(value)}`)
+                }
+                $={(self) => {
+                  self.set_increments(5, 5)
+                }}
+              />
+              <button cssClasses={["night-light-button"]}>
+                <image iconName={"night-light"} />
+              </button>
             </box>
           </box>
-        </popover>
+        </MyPopover>
       </menubutton>
     </box>
   )
